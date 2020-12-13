4f8c958a3c6caea41aae705d9f6a1a61acc94cc6
Gustavo Diaz Galeas <diazg.gustavoa@knights.ucf.edu>
2020-12-12 22:32:29 -0500

    Minor update to HelpCog for formatting purposes.

--- bot/cogs/help.py
+++ bot/cogs/help.py
@@ -29,7 +29,7 @@ class HelpCog(commands.Cog):
 
            if (cmd == "daily-alert"):
 
-               embed = "```\nPossible arguments:\n\tclear-list    Clears the Daily Watchlist \n\tadd-daily           Adds a daily to the Daily Watchlist \n\tremove-daily        Removes a daily from the Daily watchlist \n\tdisplay-watchlist       Displays the Daily Watchlist \n\tadd-notify        Add specified role to be pinged \n\tremove-notify        Remove specified role from being pinged \n\tdisplay-notify        Displays roles that will be notified```"
+               embed = "```\nPossible arguments:\n\tclear-list          Clears the Daily Watchlist \n\tadd-daily           Adds a daily to the Daily Watchlist \n\tremove-daily        Removes a daily from the Daily watchlist \n\tdisplay-watchlist   Displays the Daily Watchlist \n\tadd-notify          Add specified role to be pinged \n\tremove-notify       Remove specified role from being pinged \n\tdisplay-notify      Displays roles that will be notified```"
 
         else:
 
@@ -38,4 +38,3 @@ class HelpCog(commands.Cog):
             embed = "```\n%sdaily-alert [args...]\n\tSeries of commands used in managing daily alert notifications regarding Daily Achievements\n```" % (PREFIX)
 
         await ctx.send(response + embed)
-
