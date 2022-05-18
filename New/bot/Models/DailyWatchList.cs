2c0a46e8f768853dcd83ecd837fa8695e6b79a01
Gustavo Diaz Galeas <diazg.gustavoa@gmail.com>
2022-05-18 07:21:24 -0400

    Added document models representative of MongoDB collections

---
+++ New/bot/Models/DailyWatchList.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+#nullable enable
+
+namespace bot.Models
+{
+    /// <summary>
+    /// Model representative of the document titled 'Daily Watchlist'
+    /// </summary>
+    public class DailyWatchList
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+
+        [BsonElement("Title")]
+        public string DocTitle { get; set; } = null!;
+
+        public string[] Watchlist { get; set; } = null!;
+    }
+}
---
+++ New/bot/Models/ListOfDailies.cs
@@ -0,0 +1,25 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.Collections.Generic;
+
+#nullable enable
+
+namespace bot.Models
+{
+    /// <summary>
+    /// Model representative of the document titled 'List of Daily Achievements'
+    /// </summary>
+    public class ListOfDailies
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+
+        [BsonElement("Title")]
+        public string DocTitle { get; set; } = null!;
+
+        public string Date { get; set; } = null!;
+
+        public Dictionary<int, string> Achievements { get; set; } = null!;
+    }
+}
