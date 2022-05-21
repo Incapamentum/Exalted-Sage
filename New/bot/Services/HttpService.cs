195958a80d6c22c4d315628305fb8ad2325f63d2
Gustavo Diaz Galeas <diazg.gustavoa@gmail.com>
2022-05-21 16:23:40 -0400

    Included HttpService class definition to handle different GET/POST requests

---
+++ New/bot/Services/HttpService.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bot.Services
+{
+    public class HttpService
+    {
+        public static async Task<JObject> GetTomorrowsDailies()
+        {
+            HttpClient client = new();
+
+            HttpResponseMessage response;
+            response = await client.GetAsync("https://api.guildwars2.com/v2/achievements/daily/tomorrow");
+
+            try
+            {
+                response.EnsureSuccessStatusCode();
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine("\nException Caught!");
+                Console.WriteLine("Message: {0}", e.Message);
+
+                return null;
+            }
+
+            string responseBody = await response.Content.ReadAsStringAsync();
+            JObject json = JObject.Parse(responseBody);
+
+            return json;
+        }
+    }
+}
