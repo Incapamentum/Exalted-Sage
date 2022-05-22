08943896e3bbf7cd771d7f6bd76d6d85b3055ab0
Gustavo Diaz Galeas <diazg.gustavoa@gmail.com>
2022-05-22 14:20:46 -0400

    Rewrote class to avoid issues with static objects

--- New/bot/Services/DatabaseService.cs
+++ New/bot/Services/DatabaseService.cs
@@ -7,38 +7,62 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Bot.Config;
+
 namespace Bot.Services
 {
     /// <summary>
-    ///     Exposes the database as a service via public methods for retrieving specific
-    ///     stored documents.
+    ///     Exposes a database as a service to the program via public methods
+    ///     in the retrieval of specific stored documents
     /// </summary>
     public class DatabaseService
     {
         private readonly MongoDatabaseBase _mDatabase;
+        private readonly Dictionary<string, string> _collectionNames;
 
-        public DatabaseService(string connectionUri, string dbName)
+        /// <summary>
+        ///     Establishes a connection with the database specified by the settings,
+        ///     and collects the names of each collection in the database.
+        /// </summary>
+        /// <param name="settings">
+        ///     Contains data specific to the database in question
+        /// </param>
+        public DatabaseService(Dictionary<string, string> settings)
         {
-            var clientSettings = MongoClientSettings.FromConnectionString(connectionUri);
+            // Connecting to the database
+            var clientSettings = MongoClientSettings.FromConnectionString(settings["ConnectionUri"]);
             clientSettings.ServerApi = new ServerApi(ServerApiVersion.V1);
-
             var client = new MongoClient(clientSettings);
 
-            _mDatabase = client.GetDatabase(dbName) as MongoDatabaseBase;
+            _mDatabase = client.GetDatabase(settings["DatabaseName"]) as MongoDatabaseBase;
+
+            // Collecting names of collections that make up the database
+            _collectionNames = new Dictionary<string, string>();
+
+            var keys = settings.Keys.ToList<string>();
+
+            foreach (string k in keys)
+            {
+                if (k.Contains("Collection"))
+                {
+                    _collectionNames.Add(k, settings[k]);
+                }
+            }
         }
 
         /// <summary>
-        ///     Retrieve the achievement ID : achievement name mapping of daily achievements
+        ///     Retrieves a collection of daily achievements
         /// </summary>
         /// <returns>
-        ///     The ID : name mapping of daily achievements
+        ///     An ID:name pairing
         /// </returns>
+        /// 
         public Dictionary<int, string> GetDailyAchievements()
         {
             Dictionary<int, string> dailyAchievements = null;
             MongoCollectionBase<AchievementDoc> achievementCollection;
 
-            achievementCollection = _mDatabase.GetCollection<AchievementDoc>("achievements")
+            achievementCollection = _mDatabase.GetCollection<AchievementDoc>(_collectionNames["AchievementsCollection"])
                 as MongoCollectionBase<AchievementDoc>;
 
             var builder = Builders<AchievementDoc>.Filter;
@@ -50,12 +74,19 @@ namespace Bot.Services
             return dailyAchievements;
         }
 
+        /// <summary>
+        ///     Retrieves a watchlist of daily achievements
+        /// </summary>
+        /// <returns>
+        ///     A list of daily achievements being monitored
+        /// </returns>
         public string[] GetDailyWatchlist()
         {
             string[] dailyWatchlist = null;
+
             MongoCollectionBase<WatchlistDoc> watchlistCollection;
 
-            watchlistCollection = _mDatabase.GetCollection<WatchlistDoc>("watchlists")
+            watchlistCollection = _mDatabase.GetCollection<WatchlistDoc>(_collectionNames["WatchlistsCollection"])
                 as MongoCollectionBase<WatchlistDoc>;
 
             var builder = Builders<WatchlistDoc>.Filter;
