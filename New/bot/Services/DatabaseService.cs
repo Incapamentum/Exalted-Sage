ab1ba604e1d8bea171e5e20adad1a180bfe1225d
Gustavo Diaz Galeas <diazg.gustavoa@gmail.com>
2022-06-14 21:19:04 -0400

    Service rewritten as static, implemented async operations

--- New/bot/Services/DatabaseService.cs
+++ New/bot/Services/DatabaseService.cs
@@ -1,73 +1,58 @@
 ﻿using Bot.Models;
-using MongoDB.Bson;
 using MongoDB.Driver;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
-
-using Bot.Config;
 
 namespace Bot.Services
 {
     /// <summary>
     ///     Exposes a database as a service to the program via public methods
-    ///     in the retrieval of specific stored documents
+    ///     in the retrieval of stored documents
     /// </summary>
-    public class DatabaseService
+    public static class DatabaseService
     {
-        private readonly MongoDatabaseBase _mDatabase;
-        private readonly Dictionary<string, string> _collectionNames;
-
         /// <summary>
-        ///     Establishes a connection with the database specified by the settings,
-        ///     and collects the names of each collection in the database.
+        ///     Establishes a connection to the MongoDB cluster
+        ///     specified by the URI string
         /// </summary>
-        /// <param name="settings">
-        ///     Contains data specific to the database in question
+        /// <param name="connectionUri">
+        ///     The URI string to connect to the MongoDB service
         /// </param>
-        public DatabaseService(Dictionary<string, string> settings)
+        /// <returns></returns>
+        public static MongoClient EstablishConnection(string connectionUri)
         {
-            // Connecting to the database
-            var clientSettings = MongoClientSettings.FromConnectionString(settings["ConnectionUri"]);
+            MongoClient client = null;
+
+            var clientSettings = MongoClientSettings.FromConnectionString(connectionUri);
             clientSettings.ServerApi = new ServerApi(ServerApiVersion.V1);
-            var client = new MongoClient(clientSettings);
+            client = new MongoClient(clientSettings);
 
-            _mDatabase = client.GetDatabase(settings["DatabaseName"]) as MongoDatabaseBase;
-
-            // Collecting names of collections that make up the database
-            _collectionNames = new Dictionary<string, string>();
-
-            var keys = settings.Keys.ToList<string>();
-
-            foreach (string k in keys)
-            {
-                if (k.Contains("Collection"))
-                {
-                    _collectionNames.Add(k, settings[k]);
-                }
-            }
+            return client;
         }
 
         /// <summary>
         ///     Retrieves a collection of daily achievements
         /// </summary>
+        /// <param name="client">
+        ///     The MongoDB client connection to cluster.
+        /// </param>
         /// <returns>
-        ///     An ID:name pairing
+        ///     A {achievementID, achivementName} pairing of daily PVE achievements
         /// </returns>
-        /// 
-        public Dictionary<int, string> GetDailyAchievements()
+        public static async Task<Dictionary<int, string>> GetDailyPveAchievements(MongoClient client)
         {
             Dictionary<int, string> dailyAchievements = null;
             MongoCollectionBase<AchievementDoc> achievementCollection;
 
-            achievementCollection = _mDatabase.GetCollection<AchievementDoc>(_collectionNames["AchievementsCollection"])
+            var database = client.GetDatabase("Auric_Oasis") as MongoDatabaseBase;
+
+            achievementCollection = database.GetCollection<AchievementDoc>("achievements")
                 as MongoCollectionBase<AchievementDoc>;
 
             var builder = Builders<AchievementDoc>.Filter;
             var filter = builder.Eq("Title", "Daily Achievements");
-            var dailyAchieves = achievementCollection.Find(filter).FirstOrDefault();
+            var dailyAchieves = await achievementCollection.Find(filter).FirstOrDefaultAsync();
 
             dailyAchievements = dailyAchieves.Achievements;
 
@@ -75,23 +60,27 @@ namespace Bot.Services
         }
 
         /// <summary>
-        ///     Retrieves a watchlist of daily achievements
+        ///     Retrieves the watchlist of daily PVE achievements
         /// </summary>
+        /// <param name="client">
+        ///     The MongoDB client connection to cluster.
+        /// </param>
         /// <returns>
-        ///     A list of daily achievements being monitored
+        ///     A list of strings of the daily PVE achievement watchlist
         /// </returns>
-        public string[] GetDailyWatchlist()
+        public static async Task<string[]> GetPveDailyWatchlist(MongoClient client)
         {
             string[] dailyWatchlist = null;
-
             MongoCollectionBase<WatchlistDoc> watchlistCollection;
 
-            watchlistCollection = _mDatabase.GetCollection<WatchlistDoc>(_collectionNames["WatchlistsCollection"])
+            var database = client.GetDatabase("Auric_Oasis") as MongoDatabaseBase;
+
+            watchlistCollection = database.GetCollection<WatchlistDoc>("watchlists")
                 as MongoCollectionBase<WatchlistDoc>;
 
             var builder = Builders<WatchlistDoc>.Filter;
             var filter = builder.Eq("Title", "Daily Watchlist");
-            var dailyWatch = watchlistCollection.Find(filter).FirstOrDefault();
+            var dailyWatch = await watchlistCollection.Find(filter).FirstOrDefaultAsync();
 
             dailyWatchlist = dailyWatch.Watchlist;
 
