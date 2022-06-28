475359dbfc41c30cf53089a597c026f3fb6530d9
Gustavo Diaz Galeas <diazg.gustavoa@gmail.com>
2022-06-28 19:43:15 -0400

    Included new helper class to ensure the existence of only one Random object.

---
+++ New/bot/Helpers/RandomHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Bot.Helpers
+{
+    /// <summary>
+    ///     Static class accessible to entire application due
+    ///     to Random not being thread safe.
+    /// </summary>
+    internal static class RandomHelper
+    {
+        internal static readonly Random rand;
+
+        static RandomHelper()
+        {
+            rand = new Random(16);
+        }
+    }
+}
