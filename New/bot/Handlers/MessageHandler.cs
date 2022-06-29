ba39923e823fcabf9483282db2ed2391053ff4b5
Gustavo Diaz Galeas <diazg.gustavoa@gmail.com>
2022-06-28 20:08:54 -0400

    Private method written to handle random selection of responses.

--- New/bot/Handlers/MessageHandler.cs
+++ New/bot/Handlers/MessageHandler.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
+using Bot.Helpers;
 using Bot.Services;
 
 namespace Bot.Handlers
@@ -24,6 +25,8 @@ namespace Bot.Handlers
 
         public async Task MessageReceivedAsync(SocketMessage message)
         {
+            string response = null;
+
             var selfId = _discordClient.CurrentUser.Id;
 
             // Bot shouldn't process any messages it sends
@@ -31,41 +34,57 @@ namespace Bot.Handlers
                 return;
 
             var content = message.Content.ToLower();
-            var rand = new Random(16);
             var mentioned = new List<SocketUser>(message.MentionedUsers);
-
-            // Some of these execution paths could be made compact into the form
-            // of a function, i.e.
-            //
-            //      var response
-            //      var index
-            //      await message
-            //
-            // are all the same implementation  
+            var chance = RandomHelper.rand.NextDouble();
 
             // Respond with an appropriate message dealing with Tarir
-            if (content.Contains("tarir") && rand.NextDouble() < 0.25)
+            if (content.Contains("tarir") && chance < 0.25)
             {
-                var response = await DatabaseService.GetResponses(_mongoClient, "Tarir Responses");
-                var index = rand.Next(response.Count);
-
-                await message.Channel.SendMessageAsync(response[index]);
+                response = await RandomResponse("Tarir Responses");
             }
-
             // Respond with an appropriate message if pinged
-            if (mentioned.Count > 0)
+            else if (mentioned.Count > 0 && chance < 0.25)
             {
                 foreach (var user in mentioned)
                 {
                     if (user.Id == selfId)
                     {
-                        var response = await DatabaseService.GetResponses(_mongoClient, "Egg Bearer Responses");
-                        var index = rand.Next(response.Count);
-
-                        await message.Channel.SendMessageAsync(response[index]);
+                        response = await RandomResponse("Egg Bearer Responses");
                     }
                 }
             }
+            // Cursed responses
+            else if (chance < 0.05)
+            {
+                response = await RandomResponse("UwU Responses");
+            }
+
+            if (response != null)
+            {
+                await message.Channel.SendMessageAsync(response);
+            }
+        }
+
+        /// <summary>
+        ///     Retrieves a response doc from the database, then returns
+        ///     a randomly selected response.
+        /// </summary>
+        /// <param name="responseType">
+        ///     The title of the response doc.
+        /// </param>
+        /// <returns>
+        ///     A randomly selected response from the doc.
+        /// </returns>
+        private async Task<string> RandomResponse(string responseType)
+        {
+            string randomResponse = null;
+
+            var responses = await DatabaseService.GetResponses(_mongoClient, responseType);
+            var index = RandomHelper.rand.Next(responses.Count);
+
+            randomResponse = responses[index];
+
+            return randomResponse;
         }
     }
 }
