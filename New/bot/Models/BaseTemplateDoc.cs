80ac6576f7fd1588e9c4672ad17147c51bcbf24e
Gustavo Diaz Galeas <diazg.gustavoa@gmail.com>
2023-02-09 20:08:13 -0500

    Added a base template from which all other docs can inherit from, standardizing them across the board

---
+++ New/bot/Models/BaseTemplateDoc.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+#nullable enable
+
+namespace Bot.Models
+{
+    internal class BaseTemplateDoc
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; } = null!;
+
+        [BsonElement("Title")]
+        public string DocName { get; set; } = null!;
+
+        public string Date { get; set; } = null!;
+    }
+}
