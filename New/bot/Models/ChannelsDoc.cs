85abcc615dd78bf83f27d86fe4cc0d2fdea0e557
Gustavo Diaz Galeas <diazg.gustavoa@gmail.com>
2023-02-10 21:09:16 -0500

    Marked as obsolete. Will be removed in a future release.

--- New/bot/Models/ChannelsDoc.cs
+++ New/bot/Models/ChannelsDoc.cs
@@ -1,9 +1,12 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 #nullable enable
 
 namespace Bot.Models
 {
+    [Obsolete("The 'channels' category is planned to be removed in a future " +
+        "update, so this should not be used until it's removed")]
     internal class ChannelsDoc : BaseTemplateDoc
     {
         public Dictionary<string, ulong>? Channels { get; set; }
