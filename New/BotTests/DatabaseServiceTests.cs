73f0152ef894e48fdbd609ded7fc454251e13e66
Gustavo Diaz Galeas <diazg.gustavoa@gmail.com>
2023-02-12 00:04:43 -0500

    Introduced simple unit test that ensures responses are all non-null

--- New/BotTests/DatabaseServiceTests.cs
+++ New/BotTests/DatabaseServiceTests.cs
@@ -1,7 +1,5 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-using MongoDB.Driver;
-
 using Bot.Config;
 using Bot.Services;
 
@@ -10,13 +8,38 @@ namespace BotTests
     [TestClass]
     public class DatabaseServiceTests
     {
-        DatabaseService.SetDatabaseName();
-        readonly MongoClient _client = DatabaseService.EstablishConnection(
+        readonly DatabaseService dbService = new("Bot_Test",
             "mongodb://localhost:27017");
 
         [TestMethod]
-        public void TestMethod1()
+        public void CheckIfDocRetrievalFails()
         {
+            var taskAchieves = dbService.GetDailyPveAchievements();
+            taskAchieves.Wait();
+            var responseAchieves = taskAchieves.Result;
+
+            var taskCategories = dbService
+                .GetCategoryTextChannels("admin-tools");
+            taskCategories.Wait();
+            var responseCategories = taskCategories.Result;
+
+            var taskResponses = dbService.GetResponses("Grab Bag Responses");
+            taskResponses.Wait();
+            var responseResponses = taskResponses.Result;
+
+            var taskRoles = dbService.GetLeadershipIds();
+            taskRoles.Wait();
+            var responseRoles = taskRoles.Result;
+
+            var taskWatchlist = dbService.GetDailyPveWatchlist();
+            taskWatchlist.Wait();
+            var responseWatchlist = taskWatchlist.Result;
+
+            Assert.IsNotNull(responseAchieves);
+            Assert.IsNotNull(responseCategories);
+            Assert.IsNotNull(responseResponses);    
+            Assert.IsNotNull(responseRoles);
+            Assert.IsNotNull(responseWatchlist);
         }
     }
 }
