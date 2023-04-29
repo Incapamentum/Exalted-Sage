6d3f3cbd5d12b4d41585a28ae172f7f03d2a4f1a
Gustavo Diaz Galeas <diazg.gustavoa@gmail.com>
2023-04-29 17:34:53 -0400

    Removed old comments and updated to adhere to 80-char limit

--- New/Bot/ExaltedSage/ExaltedSage.cs
+++ New/Bot/ExaltedSage/ExaltedSage.cs
@@ -16,16 +16,15 @@ namespace Bot
 {
     class ExaltedSage
     {
-        // These are app specific
+        // Token to authenticate the bot to Discord
         private readonly string _token;
 
-        // Services
+        // Dyanmic services
         private readonly DatabaseService _databaseService;
 
-        // Client interfaces
+        // Discord-specific client and configuration
         private readonly DiscordSocketClient _discordClient;
         private readonly DiscordSocketConfig _discordConfig;
-        //private readonly MongoClient _mongoClient;
 
         // Handlers
         //private readonly SlashCommandHandler _slashCommandHandler;
@@ -40,13 +39,10 @@ namespace Bot
         public ExaltedSage(AppConfig appSettings)
         {
             _token = appSettings.settings.Token;
-            //DatabaseService.SetDatabaseName(appSettings.settings.DatabaseName);
 
             // Initializing the Discord client interface
             _discordConfig = GenerateConfig();
             _discordClient = new DiscordSocketClient(_discordConfig);
-            //_mongoClient = DatabaseService.EstablishConnection(
-            //    appSettings.settings.ConnectionUri);
 
             // Establishing connection to the database
             _databaseService = new DatabaseService(
@@ -54,6 +50,7 @@ namespace Bot
                 appSettings.settings.ConnectionUri);
 
             //_slashCommandHandler = new SlashCommandHandler();
+            // Initializing event handlers
             _guildMemberEventHandler = new GuildMemberEventHandler
                 (_discordClient, _databaseService);
             _messageHandler = new MessageEventHandler(_discordClient,
@@ -65,22 +62,19 @@ namespace Bot
 
             _logService = new LogService(_discordClient);
 
-            // Guild member events
+            // Hooking up each event handler to its corresponding listener
+            // through the client
             _discordClient.GuildMemberUpdated 
                 += _guildMemberEventHandler.GuildMemberUpdatedAsync;
-
-            //_discordClient.SlashCommandExecuted += _slashCommandHandler.SlashCommandExecute;
-            // Message events
-            _discordClient.MessageReceived += _messageHandler.MessageReceivedAsync;
-            _discordClient.MessageDeleted += _messageHandler.MessageDeletedAsync;
-
-            // User events
+            _discordClient.MessageReceived
+                += _messageHandler.MessageReceivedAsync;
+            _discordClient.MessageDeleted
+                += _messageHandler.MessageDeletedAsync;
             _discordClient.UserLeft += _userHandler.UserLeftAsync;
-
-            // Voice channel events
             _discordClient.UserVoiceStateUpdated 
                 += _voiceHandler.VoiceStateChangeAsync;
 
+            // Adding the log service to post status message when ready
             _discordClient.Log += _logService.LogAsync;
 
             _discordClient.Ready += ReadyAsync;
@@ -110,8 +104,8 @@ namespace Bot
         }
 
         /// <summary>
-        ///     Executed hourly to check if server reset has been reached (occurs at 00:00 UTC)
-        ///     to run additional tasks
+        ///     Executed hourly to check if server reset has been reached
+        ///     (occurs at 00:00 UTC) to run additional tasks
         /// </summary>
         private async Task OnServerReset()
         {
@@ -123,24 +117,29 @@ namespace Bot
             else
                 channelId = 720690834638372949;
 
-            var broadcastChannel = _discordClient.GetChannel(channelId) as SocketTextChannel;
+            var broadcastChannel = _discordClient.GetChannel(channelId)
+                as SocketTextChannel;
             var utcNow = DateTime.UtcNow;
 
             // TODO: this may require some clean-up.
             if (utcNow.Hour == 0)
             {
                 // Different collections to filter results from
-                var tomorrowPveDailiesId = await HttpService.GetTomorrowsPveDailiesId();
-                var dailyPveAchievements = await _databaseService.
-                    GetDailyPveAchievements();
-                var dailyPveWatchlist = await _databaseService.
-                    GetDailyPveWatchlist();
+                var tomorrowPveDailiesId = await HttpService
+                    .GetTomorrowsPveDailiesId();
+                var dailyPveAchievements = await _databaseService
+                    .GetDailyPveAchievements();
+                var dailyPveWatchlist = await _databaseService
+                    .GetDailyPveWatchlist();
 
                 List<string> dailyPveNames = new();
                 List<string> upcomingPveDailies = new();
 
-                dailyPveNames = AchievementHelper.AchievementNamesFromIds(tomorrowPveDailiesId, dailyPveAchievements);
-                upcomingPveDailies = AchievementHelper.AchievementsSetToAppear(dailyPveNames, dailyPveWatchlist);
+                dailyPveNames = AchievementHelper
+                    .AchievementNamesFromIds(tomorrowPveDailiesId,
+                    dailyPveAchievements);
+                upcomingPveDailies = AchievementHelper
+                    .AchievementsSetToAppear(dailyPveNames, dailyPveWatchlist);
 
                 // List is non-empty; proceed to build an embedded message
                 if (upcomingPveDailies.Count > 0)
@@ -148,18 +147,22 @@ namespace Bot
                     var embed = new EmbedBuilder
                     {
                         Title = "Daily Alert",
-                        Description = "Attention! A daily achievement that is being monitored will appear tomorrow!",
+                        Description = "Attention! A daily achievement that is" +
+                        " being monitored will appear tomorrow!",
                         Color = 0xffee05
                     };
 
                     foreach (string achieveName in upcomingPveDailies)
                     {
-                        var achieveId = AchievementHelper.AchievementGetIdFromName(achieveName, dailyPveAchievements);
+                        var achieveId = AchievementHelper
+                            .AchievementGetIdFromName(achieveName,
+                            dailyPveAchievements);
 
                         // Valid ID check
                         if (achieveId != 0)
                         {
-                            var desc = await HttpService.ObtainAchievementDescription(achieveId);
+                            var desc = await HttpService
+                                .ObtainAchievementDescription(achieveId);
 
                             embed.AddField(achieveName, desc);
                         }
@@ -190,8 +193,8 @@ namespace Bot
         /// </param>
         /// <param name="cancellationToken"></param>
         /// 
-        private static async Task PeriodicAsync(Func<Task> action, TimeSpan interval,
-            CancellationToken cancellationToken = default)
+        private static async Task PeriodicAsync(Func<Task> action,
+            TimeSpan interval, CancellationToken cancellationToken = default)
         {
             using var timer = new PeriodicTimer(interval);
             while (await timer.WaitForNextTickAsync(cancellationToken))
